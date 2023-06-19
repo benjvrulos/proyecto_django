73d1abef84303bf82a1b22cba4d44057a58f26e5
benjv_tdh <cortesbenja@gmail.com>
2023-06-18 22:41:05 -0400

    mantenedor view

--- petMatter/apps/Tienda/models.py
+++ petMatter/apps/Tienda/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    id_categoria = models.IntegerField(primary_key=True)
+    nombre_categoria = models.CharField(max_length=100)
+
+    def __str__(self):
+        return 'Nombre: {} - ID: {}'.format(self.id_categoria,self.nombre_categoria)
+    
+class Producto(models.Model):
+    sku = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    stock = models.IntegerField()
+    precio = models.IntegerField()
+    descripcion = models.CharField(max_length=250)
+    id_categoria = models.ForeignKey(Categoria,on_delete=models.CASCADE)
+    imagen = models.ImageField(upload_to='imagenesProducto')
+
+    def __str__(self):
+        return 'N°: {} - Stock: {} - Nombre: {}'.format(self.sku,self.stock,self.nombre)
+    
--- petMatter/apps/Tienda/views.py
+++ petMatter/apps/Tienda/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Producto
 
 def index(request):
     return render(request,'Tienda/index.html')
+
+def mantenedor(request):
+    productos = Producto.objects.all()
+    return render(request,'Tienda/mantenedor.html',{'productos':productos})
